b3b12a1e75b0f8d94c3253977d1fdb96437a48f2
Bryan Walsh <bpwalsh90@gmail.com>
2020-06-05 20:27:47 -0400

    Initial Commit

---
+++ WingspanScout/app/src/main/java/com/example/wingspanscout/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.wingspanscout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
